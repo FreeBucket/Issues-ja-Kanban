813d5175764749c59d4482d27b3280f6526d13e1
SeYksTyyppiSielJossakin <jakehirvi27@gmail.com>
2024-11-27 10:03:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ document.getElementById('addTaskButton').addEventListener('click', function() {
         const li = document.createElement('li');
         li.textContent = taskText;
 
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Poista';
+        deleteButton.classList.add('deleteButton');
+        
+        // Lisää poista-painike listan kohtaan
+        li.appendChild(deleteButton);
+
+        // Lisää tehtävä listaan
         document.getElementById('numeroc').appendChild(li);
+
+        // Kuuntelija poista-painikkeelle
+        deleteButton.addEventListener('click', function() {
+            li.remove();
+        });
 
         // Tyhjennä syötekenttä
         taskInput.value = "";
